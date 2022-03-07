0c8881ad0736c2d3e9263ab49a2af19e0e9a76b8
musa.adsiz <musa.adsiz@netix.com.tr>
2022-03-08 00:24:41 +0300

    Add project files.

---
+++ TestHeartbeatWindowsService/Heartbeat.cs
@@ -0,0 +1,37 @@
+﻿
+using timer = System.Timers;
+namespace TestHeartbeatWindowsService
+{
+    public class Heartbeat
+    {
+        private readonly timer.Timer timer;
+
+        public Heartbeat()
+        {
+            timer = new timer.Timer(1000) { AutoReset = true };
+            timer.Elapsed += TimerElapsed;
+        }
+
+        private void TimerElapsed(object? sender, timer.ElapsedEventArgs e)
+        {
+            string[] lines = new string[]
+            {
+                DateTime.Now.ToString()
+
+            };
+            File.AppendAllLines(@"C:\Musa\deneme.txt", lines);
+        }
+        public void Start()
+        {
+            timer.Start();
+        }
+        public void Stop()
+        {
+            if (timer.Enabled == true)
+            {
+                timer.Stop();
+            }
+
+        }
+    }
+}
---
+++ TestHeartbeatWindowsService/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+using TestHeartbeatWindowsService;
+using Topshelf;
+
+var exitCode = HostFactory.Run(s =>
+{
+    s.Service<Heartbeat>(a =>
+    {
+        a.ConstructUsing(srv => new Heartbeat());
+        a.WhenStarted(srv => srv.Start());
+        a.WhenStopped(srv => srv.Stop());
+    });
+    s.RunAsLocalSystem();
+    s.SetServiceName("MusaAdsizService");
+    s.SetDisplayName("Musa Adsiz Service");
+    s.SetDescription("This is the service with topshelf");
+
+});
+
+int exitCodeValue = (int)Convert.ChangeType(exitCode, exitCode.GetTypeCode());
+Environment.Exit(exitCodeValue);
